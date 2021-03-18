61a5f0ad3384214e8e136dad6d66ed7d8b80750e
Tharun Viswanathan <tharun.tvishy@gmail.com>
2021-03-18 19:26:56 +0530

    setup

---
+++ bot/cogs/reddit.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+import asyncpraw
+from dotenv import load_dotenv
+import os
+
+
+client_id = os.getenv("REDDIT_APP_ID")
+client_secret = os.getenv("REDDIT_APP_SECRET")
+reddit = asyncpraw.Reddit(client_id=client_id, client_secret=client_secret,
+                          user_agent="discord:com.example.CloverBot:1.0 (by u/RedditUser762005)")
+
+
+class Reddit(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="meme", pass_context=True, help="Take a random meme from reddit")
+    async def meme(self, ctx):
+        subreddit = await reddit.subreddit("memes+dankmemes+wholesomememes")
+        submission = await subreddit.random()
+        url = submission.url
+        rEmbed = discord.Embed(
+            title=submission.title, description=submission.selftext, colour=discord.Colour.green())
+        if url.endswith(('.jpg', '.png', '.gif', '.jpeg')):
+            rEmbed.set_image(url=url)
+        rEmbed.set_author(name=submission.author)
+        rEmbed.set_footer(
+            text=f"{submission.score} ⬆️│{submission.num_comments} 💬")
+        await ctx.send(embed=rEmbed)
+
+    @commands.command(name="randompost", pass_context=True, help="Obtain a random post from a random subreddit")
+    async def randompost(self, ctx, subreddit=None):
+        if subreddit is None:
+            return await ctx.send("Please send a subreddit to obtain a post from")
+        subreddit = await reddit.subreddit(str(subreddit))
+        submission = await subreddit.random()
+        url = submission.url
+        rEmbed = discord.Embed(
+            title=submission.title, description=submission.selftext, colour=discord.Colour.green())
+        if url.endswith(('.jpg', '.png', '.gif', '.jpeg')):
+            rEmbed.set_image(url=url)
+        rEmbed.set_author(name=str(submission.author))
+        await ctx.send(embed=rEmbed)
+
+
+def setup(bot):
+    bot.add_cog(Reddit(bot))
