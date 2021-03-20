7e528e1f2c02d582c1df95cc20a3deadebf25a99
TV12mac <tharun.tvishy@gmail.com>
2021-03-20 18:59:28 +0530

    added rudimentary music capabilities

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -1,10 +1,11 @@
-import discord
-from discord.ext import commands
-import wavelink
-import typing as t
-import re
-import datetime as dt
 import asyncio
+import datetime as dt
+import re
+import typing as t
+
+import discord
+import wavelink
+from discord.ext import commands
 
 URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 OPTIONS = {
@@ -18,24 +19,33 @@ OPTIONS = {
 
 class AlreadyConnectedToChannel(commands.CommandError):
     pass
-
-
 class NoVoiceChannel(commands.CommandError):
     pass
-
-
 class QueueIsEmpty(commands.CommandError):
     pass
-
-
 class NoTracksFound(commands.CommandError):
     pass
-
 
 class Queue:
     def __init__(self):
         self._queue = []
         self.position = 0
+
+    @property
+    def is_empty(self):
+        return not self._queue
+
+    def add(self, *args):
+        self._queue.extend(args)
+
+class Queue:
+    def __init__(self):
+        self._queue = []
+        self.position = 0
+
+    @property
+    def is_empty(self):
+        return not self._queue
 
     def add(self, *args):
         self._queue.extend(args)
@@ -45,56 +55,47 @@ class Queue:
         if not self._queue:
             raise QueueIsEmpty
         return self._queue[0]
-
     def get_next_track(self):
         if not self._queue:
             raise QueueIsEmpty
-
         self.position += 1
-
         if self.position > len(self._queue) - 1:
             return None
-
         return self._queue[self.position]
-
 
 class Player(wavelink.Player):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.queue = Queue()
-
     async def connect(self, ctx, channel=None):
         if self.is_connected:
             raise AlreadyConnectedToChannel
-
         if (channel := getattr(ctx.author.voice, "channel", channel)) is None:
             raise NoVoiceChannel
-
         await super().connect(channel.id)
         return channel
-
     async def teardown(self):
         try:
             await self.destroy()
         except KeyError:
             pass
-
     async def add_tracks(self, ctx, tracks):
         if not tracks:
             raise NoTracksFound
-
         if isinstance(tracks, wavelink.TrackPlaylist):
             self.queue.add(*tracks.tracks)
         elif len(tracks) == 1:
             self.queue.add(tracks[0])
             await ctx.send(f"Added {tracks[0].title} to the queue.")
         else:
-            if(track := await self.choose_track(ctx, tracks)) is not None:
+            pass
+            if (track := await self.choose_track(ctx, tracks)) is not None:
                 self.queue.add(track)
-                await ctx.send(f"Added {track.title} to the queue")
+                await ctx.send(f"Added {track.title} to the queue.")
 
         if not self.is_playing:
-            await self.start_playback()
+            if not self.is_playing and not self.queue.is_empty:
+                await self.start_playback()
 
     async def choose_track(self, ctx, tracks):
         def _check(r, u):
@@ -104,23 +105,21 @@ class Player(wavelink.Player):
                 and r.message.id == msg.id
             )
 
-        mEmbed = discord.Embed(
+        embed = discord.Embed(
             title="Choose a song",
             description=(
                 "\n".join(
-                    f"**{i + 1}.**{t.title} ({t.length//60000}:{str(t.length%60).zfill(2)})"
+                    f"**{i+1}.** {t.title} ({t.length//60000}:{str(t.length%60).zfill(2)})"
                     for i, t in enumerate(tracks[:5])
                 )
             ),
             colour=ctx.author.colour,
             timestamp=dt.datetime.utcnow()
         )
-        mEmbed.set_author(name="Query Results")
-        mEmbed.set_footer(
-            text=f"Invoked by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
+        embed.set_author(name="Query Results")
+        embed.set_footer(text=f"Invoked by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
 
-        msg = await ctx.send(embed=mEmbed)
-
+        msg = await ctx.send(embed=embed)
         for emoji in list(OPTIONS.keys())[:min(len(tracks), len(OPTIONS))]:
             await msg.add_reaction(emoji)
 
@@ -142,33 +141,31 @@ class Player(wavelink.Player):
                 await self.play(track)
         except QueueIsEmpty:
             pass
-
-
 class Music(commands.Cog, wavelink.WavelinkMixin):
     def __init__(self, bot):
         self.bot = bot
         self.wavelink = wavelink.Client(bot=bot)
         self.bot.loop.create_task(self.start_nodes())
-
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if not member.bot and after.channel is None:
             if not [m for m in before.channel.members if not m.bot]:
                 await self.get_player(member.guild).teardown()
-
     @wavelink.WavelinkMixin.listener()
     async def on_node_ready(self, node):
-        print(f"Wavelink Node `{node.identifier}` ready.")
-
+        print(f" Wavelink node `{node.identifier}` ready.")
+    @wavelink.WavelinkMixin.listener("on_track_stuck")
+    @wavelink.WavelinkMixin.listener("on_track_end")
+    @wavelink.WavelinkMixin.listener("on_track_exception")
+    async def on_player_stop(self, node, payload):
+        await payload.player.advance()
     async def cog_check(self, ctx):
         if isinstance(ctx.channel, discord.DMChannel):
-            await ctx.send("Commands are not available in DMs")
+            await ctx.send("Music commands are not available in DMs.")
             return False
         return True
-
     async def start_nodes(self):
         await self.bot.wait_until_ready()
-
         nodes = {
             "MAIN": {
                 "host": "127.0.0.1",
@@ -176,63 +173,43 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
                 "rest_uri": "http://127.0.0.1:2333",
                 "password": "youshallnotpass",
                 "identifier": "MAIN",
-                "region": "asia",
+                "region": "europe",
             }
         }
-
         for node in nodes.values():
             await self.wavelink.initiate_node(**node)
-
     def get_player(self, obj):
         if isinstance(obj, commands.Context):
             return self.wavelink.get_player(obj.guild.id, cls=Player, context=obj)
-
         elif isinstance(obj, discord.Guild):
             return self.wavelink.get_player(obj.id, cls=Player)
-
     @commands.command(name="connect", aliases=["join"])
-    async def connect_cmd(self, ctx, *, channel: t.Optional[discord.VoiceChannel]):
+    async def connect_command(self, ctx, *, channel: t.Optional[discord.VoiceChannel]):
         player = self.get_player(ctx)
         channel = await player.connect(ctx, channel)
-        await ctx.send(f"connected to {channel.name}")
-
-    @connect_cmd.error
-    async def connect_cmd_error(self, ctx, exc):
+        await ctx.send(f"Connected to {channel.name}.")
+    @connect_command.error
+    async def connect_command_error(self, ctx, exc):
         if isinstance(exc, AlreadyConnectedToChannel):
-            await ctx.send("Already connected to a voice channel")
-
+            await ctx.send("Already connected to a voice channel.")
         elif isinstance(exc, NoVoiceChannel):
-            await ctx.send("No suitable voice channel was found")
-
+            await ctx.send("No suitable voice channel was provided.")
     @commands.command(name="disconnect", aliases=["leave"])
     async def disconnect_command(self, ctx):
         player = self.get_player(ctx)
         await player.teardown()
-        await ctx.send("Disconnected player from channel")
-
-    @commands.command(name="play", help="Play some music")
-    async def play(self, ctx, *, query: t.Optional[str]):
+        await ctx.send("Disconnect.")
+    @commands.command(name="play")
+    async def play_command(self, ctx, *, query: t.Optional[str]):
         player = self.get_player(ctx)
-
         if not player.is_connected:
             await player.connect(ctx)
-
         if query is None:
             pass
-
         else:
             query = query.strip("<>")
             if not re.match(URL_REGEX, query):
                 query = f"ytsearch:{query}"
-
             await player.add_tracks(ctx, await self.wavelink.get_tracks(query))
-
-    @wavelink.WavelinkMixin.listener("on_track_stuck")
-    @wavelink.WavelinkMixin.listener("on_track_end")
-    @wavelink.WavelinkMixin.listener("on_track_exception")
-    async def on_player_stop(self, node, payload):
-        await payload.player.advance()
-
-
 def setup(bot):
     bot.add_cog(Music(bot))
---
+++ bot/cogs/news.py
@@ -0,0 +1,70 @@
+import discord
+from discord.ext import commands
+from googletrans import Translator
+import json
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+translator = Translator()
+
+api_key = os.getenv('NEWSAPI_KEY')
+
+
+class News(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="countrynews", aliases=['cnews', 'cn'], pass_context=True, help="Find the top news of a country")
+    async def cnews(self, ctx, country: str = None, count: int = None):
+        if country is None:
+            return await ctx.send("Please provide a country.")
+        if count is None:
+            return await ctx.send("Please provide some number of articles")
+        url = f"https://newsapi.org/v2/top-headlines?country={country.lower()}&apiKey={api_key}"
+        responses = requests.get(url)
+        articles = json.loads(responses.text)
+        i = 1
+        if articles['articles'] == []:
+            return await ctx.send("There are no news articles for this country")
+
+        for article in articles['articles']:
+            nEmbed = discord.Embed(
+                title=translator.translate(article['title']).text, description=translator.translate(article['description']).text, colour=discord.Colour.blurple())
+            nEmbed.set_author(name=article['author'])
+            nEmbed.set_image(url=article['urlToImage'])
+            await ctx.send(embed=nEmbed)
+            if i < count:
+                i += 1
+            else:
+                break
+
+    @commands.command(name="topicnews", aliases=['tnews', 'tn'], pass_context=True, help="Find news based on a query")
+    async def tnews(self, ctx, query: str = None, count: int = None):
+        if query is None:
+            return await ctx.send("You haven't sent a query to search for the news on")
+        if count is None:
+            return await ctx.send("You haven't given a number of articles to search through")
+        url = f"https://newsapi.org/v2/everything?q={query}&apiKey={api_key}"
+        responses = requests.get(url)
+        articles = json.loads(responses.text)
+        i = 1
+        if articles['articles'] == []:
+            return await ctx.send("There are no news articles for this query")
+
+        for article in articles['articles']:
+            nEmbed = discord.Embed(
+                title=translator.translate(article['title']).text, description=translator.translate(article['description']).text, colour=discord.Colour.blurple())
+            nEmbed.set_author(name=article['author'])
+            nEmbed.set_image(url=article['urlToImage'])
+            await ctx.send(embed=nEmbed)
+            if i < count:
+                i += 1
+            else:
+                break
+
+
+def setup(bot):
+    bot.add_cog(News(bot))
