e7330414771a981fc1a10f5b897112ff4808652d
Tharun Viswanathan <tharunviswanathan@Tharuns-MacBook-Pro.local>
2021-04-03 12:14:34 +0530

    adding better music functionality

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -33,6 +33,19 @@ class NoTracksFound(commands.CommandError):
     pass
 
 
+class PlayerIsAlreadyPaused(commands.CommandError):
+    pass
+
+
+class PlayerIsAlreadyPlaying(commands.CommandError):
+    pass
+
+class NoMoreTracks(commands.CommandError):
+    pass
+
+class NoPreviousTracks(commands.CommandError):
+    pass
+
 class Queue:
     def __init__(self):
         self._queue = []
@@ -238,7 +251,7 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
     async def disconnect_command(self, ctx):
         player = self.get_player(ctx)
         await player.teardown()
-        await ctx.send("Disconnect.")
+        await ctx.send("Disconnected from channel")
 
     @commands.command(name="play", pass_context=True, help="Play a song")
     async def play_command(self, ctx, *, query: t.Optional[str]):
@@ -246,27 +259,78 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
         if not player.is_connected:
             await player.connect(ctx)
         if query is None:
-            pass
+            if not player.is_paused:
+                raise PlayersIsAlreadyPlaying
+
+            if player.queue.is_empty:
+                raise QueueIsEmpty
+
+            await player.set_pause(False)
+            await ctx.send("Playback resumed")
         else:
             query = query.strip("<>")
             if not re.match(URL_REGEX, query):
                 query = f"ytsearch:{query}"
             await player.add_tracks(ctx, await self.wavelink.get_tracks(query))
 
+    @play_command.error
+    async def play_command_error(self, ctx, exc):
+        if isinstance(exc, NoTracksFound):
+            await ctx.send("No tracks were found for this")
+
+    @commands.command(name="pause")
+    async def pause(self, ctx):
+        player = self.get_player(ctx)
+        if player.is_paused:
+            raise PlayerIsAlreadyPaused
+
+        await player.set_pause(True)
+        await ctx.send("Playback paused")
+
+    @pause.error
+    async def pause__error(ctx, exc):
+        if isinstance(exc, PlayerIsAlreadyPaused):
+            await ctx.send("Player has already been paused")
+
+
+    @commands.command(name="resume")
+    async def resume(self, ctx):
+        player = self.get_player(ctx)
+
+        if player.is_playing:
+            raise PlayerIsAlreadyPlaying
+
+        await player.set_pause(False)
+        await ctx.send("Playback resumed")
+
+    @resume.error
+    async def resume_command_error(self, ctx, exc):
+        if isinstance(exc, PlayerIsAlreadyPlaying):
+            await ctx.send("Player is already playing")
+
+    @commands.command(name="stop")
+    async def stop_command(self, ctx):
+        player = self.get_player
+        player.queue.empty()
+        await player.stop()
+        await ctx.send("Player has been stopped")
+
     @commands.command(name="queue", pass_context=True, help="Show the queue the playlist")
-    async def queue_command(self, ctx, show : t.Optional[int] = 10):
+    async def queue_command(self, ctx, show: t.Optional[int] = 10):
         player = self.get_player(ctx)
         if player.queue.is_empty:
             raise QueueIsEmpty
         qEmbed = discord.Embed(
             title="Queue",
             description=f"Showing next [show] tracks",
-            colour=ctx.author.colour(),
+            colour=ctx.author.colour,
             timestamp=dt.datetime.utcnow()
         )
         qEmbed.set_author(name="Query Results")
-        qEmbed.set_footer(text=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
-        qEmbed.add_field(name="Currently Playing: ",value=player.queue.current_track.title, inline=False)
+        qEmbed.set_footer(
+            text=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
+        qEmbed.add_field(name="Currently Playing: ",
+                         value=player.queue.current_track.title, inline=False)
         if upcoming := player.queue.upcoming:
             qEmbed.add_field(
                 name="Upcoming:",
@@ -279,6 +343,42 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
     async def queue_command_error(self, ctx, exc):
         if isinstance(exc, QueueIsEmpty):
             await ctx.send("The queue is currently empty")
+
+
+    @commands.command(name="next", aliases=['skip'])
+    async def next(self, ctx):
+        player = self.get_player(ctx)
+
+        if not player.queue.upcoming:
+            raise NoMoreTracks
+
+        await player.stop()
+        await ctx.send("The next track is playing now")
+
+    @next.error
+    async def next_error(self, ctx, exc):
+        if isinstance(ctx, NoMoreTracks):
+            await ctx.send("There are no more tracks in the queue")
+        if isinstance(ctx, QueueIsEmpty):
+            await ctx.send("The queue is empty")
+
+    @commands.command(name="previous", aliases=['back'])
+    async def back(self, ctx):
+        player = self.get_player(ctx)
+
+        if not player.queue.history:
+            raise NoPreviousTracks
+
+        player.queue_position -= 2
+        await player.stop()
+        await ctx.send("Playing the previous song in the queue")
+
+    @back.error
+    async def next_error(self, ctx, exc):
+        if isinstance(ctx, NoMoreTracks):
+            await ctx.send("There are no previous tracks in the queue")
+        if isinstance(ctx, QueueIsEmpty):
+            await ctx.send("The queue is empty")
 
 def setup(bot):
     bot.add_cog(Music(bot))
