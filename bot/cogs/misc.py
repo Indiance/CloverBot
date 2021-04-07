9725c5f2383e9ecc981546d4419ab87a54763dfa
TV12mac <tharun.tvishy@gmail.com>
2021-04-07 17:17:45 +0530

    fixing bugs related to music and miscallenous stuff

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 from random import choice
 from dotenv import load_dotenv
-import os
-
+import aiohttp
+import io
 
 load_dotenv()
 
@@ -11,8 +11,9 @@ load_dotenv()
 class Miscellaneous(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.bot.remove_command("help")
 
-    @commands.command(name='8ball', pass_context=True, help="Predict something with our 8ball.")
+    @commands.command(name='8ball', pass_context=True)
     async def ball(self, ctx, *question):
         choices = [
             "As I see it, yes", "Ask again later", "Better not tell you now",
@@ -27,24 +28,196 @@ class Miscellaneous(commands.Cog):
         else:
             await ctx.send('{}? {}'.format(' '.join(question), answer))
 
-    @commands.command(name='version', pass_context=True, help="Get the version of this bot")
-    async def versionembed(self, ctx):
-        vEmbed = discord.Embed(title="Current Version",
-                               description="Version 0.0.2",
-                               color=0x00ff00)
-        vEmbed.add_field(name="Date of Release", value="13th December 2020",
-                         inline=False)
-        vEmbed.add_field(name="Changes since previous version",
-                         value="Warn category: warn a user, clear warns and show warns")
-        vEmbed.set_footer(text="Still in development")
-        vEmbed.set_author(name="Indiance#2326")
-        await ctx.send(embed=vEmbed)
-
     @commands.command(name='shutdown', pass_context=True, help="Shutdown the bot")
     @commands.is_owner()
     async def shutdown(self, ctx):
         await ctx.bot.logout()
 
+    @commands.command(name='wasted', pass_context=True)
+    async def wasted(self, ctx, member: discord.Member = None):
+        if not member:
+            member = ctx.author
+
+        wastedsession = aiohttp.ClientSession()
+        async with wastedsession.get(f"https://some-random-api.ml/canvas/wasted?avatar={member.avatar_url_as(format='png')}") as img:
+            if img.status != 200:
+                await ctx.send("Unable to obtain image")
+                await wastedsession.close()
+            else:
+                data = io.BytesIO(await img.read())
+                await ctx.send(file=discord.File(data, 'wasted.png'))
+                await wastedsession.close()
+
+    @commands.command(name="invert", pass_context=True)
+    async def invert(self, ctx, member: discord.Member = None):
+        if not member:
+            member = ctx.author
+
+        invertsession = aiohttp.ClientSession()
+        async with invertsession.get(f"https://some-random-api.ml/canvas/invert?avatar={member.avatar_url_as(format='png')}") as img:
+            if img.status != 200:
+                await ctx.send("Unable to obtain image")
+                await invertsession.close()
+            else:
+                data = io.BytesIO(await img.read())
+                await ctx.send(file=discord.File(data, 'inverted.png'))
+                await invertsession.close()
+
+    @commands.command(name="pixelate", pass_context=True)
+    async def pixelate(self, ctx, member: discord.Member = None):
+        if not member:
+            member = ctx.author
+
+        invertsession = aiohttp.ClientSession()
+        async with invertsession.get(f"https://some-random-api.ml/canvas/pixelate?avatar={member.avatar_url_as(format='png')}") as img:
+            if img.status != 200:
+                await ctx.send("Unable to obtain image")
+                await invertsession.close()
+            else:
+                data = io.BytesIO(await img.read())
+                await ctx.send(file=discord.File(data, 'pixelated.png'))
+                await invertsession.close()
+
+    @commands.group(invoke_without_command=True)
+    async def help(self, ctx):
+        hEmbed = discord.Embed(title="Help",
+                               description="Use the +help (command) to learn more about a specif command. anything in angular brackets <> is mandatory and anything in [] is optional",
+                               colour=ctx.author.color)
+        hEmbed.add_field(name=":hammer: Moderation",
+                         value="kick, ban, unban, warn, clearwarns, warnings",
+                         inline=False)
+        hEmbed.add_field(name=":game_die: Miscellaneous",
+                         value="8ball, wasted, invert, pixelate",
+                         inline=False)
+        hEmbed.add_field(name=":desktop: Reddit",
+                         value="meme, randompost",
+                         inline=False)
+        hEmbed.add_field(name=":newspaper: News",
+                         value="countrynews, topicnews",
+                         inline=False)
+        hEmbed.add_field(name=":headphones: Music",
+                         value="connect, disconnect, play, queue",
+                         inline=False)
+        await ctx.send(embed=hEmbed)
+
+    @help.command()
+    async def kick(self, ctx):
+        em = discord.Embed(title="Kick",
+                            description="kick a member from the server",
+                            color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+kick <member> [reason]")
+        await ctx.send(embed=em)
+
+    @help.command()
+    async def ban(self, ctx):
+        em = discord.Embed(title="Ban",
+                            description="Ban a member from the server",
+                            color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+ban <member> [reason]")
+        await ctx.send(embed=em)
+
+    @help.command()
+    async def unban(self, ctx):
+        em = discord.Embed(title="Unban",
+                            description="Unban a member from the server",
+                            color=ctx.author.color)
+        em.add_field(name="**Syntax**",
+                      value="+unban <member>#<discriminator>")
+        await ctx.send(embed=em)
+
+    @help.command()
+    async def warn(self, ctx):
+        wEm = discord.Embed(title="Warn",
+                            description="Unban a member from the server",
+                            color=ctx.author.color)
+        wEm.add_field(name="**Syntax**", value="+warn <member> [reason]")
+        await ctx.send(embed=wEm)
+
+    @help.command()
+    async def warnings(self, ctx):
+        em = discord.Embed(title="Warnings",
+                             description="Show the warnings that a member has",
+                             color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+warnings <member>")
+        await ctx.send(embed=em)
+
+    @help.command()
+    async def clearwarns(self, ctx):
+        em = discord.Embed(title="Clearwarns",
+                             description="Clear the warnings that a user has",
+                             color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+clearwarns <member>")
+        await ctx.send(embed=em)
+
+    @help.command(name="8ball")
+    async def _8ball(self, ctx):
+        em = discord.Embed(title="8ball",
+                           description="Answer your questions using the 8ball.",
+                           color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+8ball <question>")
+        await ctx.send(embed=em)
+
+    @help.command(name="invert")
+    async def help_invert(self, ctx):
+        em = discord.Embed(
+            title="Invert", description="Invert a person's avatar", color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+invert [member]")
+        await ctx.send(embed=em)
+
+    @help.command(name="Pixelate")
+    async def help_wasted(self, ctx):
+        em = discord.Embed(
+            title="Pixelate", description="Pixelate someone's profile picture", color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+pixelate [member]")
+        await ctx.send(embed=em)
+
+    @help.command(name="meme")
+    async def reddit_meme(self, ctx):
+        em = discord.Embed(
+            title="Meme", description="Take a random meme from reddit", color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+meme")
+        await ctx.send(embed=em)
+
+    @help.command(name="randompost")
+    async def reddit_randompost(self, ctx):
+        em = discord.Embed(
+            title="random post", description="obtain a random post from reddit given a subreddit name", color=ctx.author.color)
+        em.add_field(name="**syntax**", value="+randompost <subreddit>")
+        await ctx.send(embed=em)
+
+    @help.command()
+    async def countrynews(self, ctx):
+        em = discord.Embed(
+            title="Countrynews", description="Pick some news from a country. The country name should be two letters", color=ctx.author.color)
+        em.add_field(name="**syntax**",
+                     value="+countrynews <country> <number of articles>")
+        await ctx.send(embed=em)
+
+    @help.command()
+    async def topicnews(self, ctx):
+        em = discord.Embed(
+            name="Topic news", description="Get some news from various sources given a topic", color=ctx.author.color)
+        em.add_field(name="**Syntax**",
+                     value="+topicnews <topic> <number of articles>")
+        await ctx.send(embed=em)
+
+    @help.command()
+    async def connect(self, ctx):
+        em = discord.Embed(
+            title="Connect", description="Join a voice channel", color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="+connect [voice channel name]")
+        await ctx.send(embed=em)
+    
+    @help.command()
+    async def play(self, ctx):
+        em = discord.Embed(title="Play", description="Play a song", color=ctx.author.color)
+        em.add_field(name="**Syntax**",value="+play <song name> or +play <url>")
+        await ctx.send(embed=em)
+    
+    async def queue(self, ctx):
+        em = discord.Embed(title="Queue", description="Show the queue in a song", color=ctx.author.color)
+        em.add_field(name="**Syntax",value="+queue")
+        await ctx.send(embed=em)
 
 def setup(bot):
     bot.add_cog(Miscellaneous(bot))
--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -2,11 +2,9 @@ import asyncio
 import datetime as dt
 import re
 import typing as t
-
 import discord
 import wavelink
 from discord.ext import commands
-
 URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 OPTIONS = {
     "1️⃣": 0,
@@ -19,13 +17,33 @@ OPTIONS = {
 
 class AlreadyConnectedToChannel(commands.CommandError):
     pass
+
+
 class NoVoiceChannel(commands.CommandError):
     pass
+
+
 class QueueIsEmpty(commands.CommandError):
     pass
+
+
 class NoTracksFound(commands.CommandError):
     pass
 
+
+class PlayerIsAlreadyPaused(commands.CommandError):
+    pass
+
+
+class PlayerIsAlreadyPlaying(commands.CommandError):
+    pass
+
+class NoMoreTracks(commands.CommandError):
+    pass
+
+class NoPreviousTracks(commands.CommandError):
+    pass
+
 class Queue:
     def __init__(self):
         self._queue = []
@@ -34,27 +52,38 @@ class Queue:
     @property
     def is_empty(self):
         return not self._queue
-
-    def add(self, *args):
-        self._queue.extend(args)
-
-class Queue:
-    def __init__(self):
-        self._queue = []
-        self.position = 0
-
-    @property
-    def is_empty(self):
-        return not self._queue
-
-    def add(self, *args):
-        self._queue.extend(args)
 
     @property
     def first_track(self):
         if not self._queue:
             raise QueueIsEmpty
         return self._queue[0]
+
+    @property
+    def current_track(self):
+        if not self._queue:
+            raise QueueIsEmpty
+        return self._queue[self.position]
+
+    @property
+    def upcoming(self):
+        if not self._queue:
+            raise QueueIsEmpty
+        return self._queue[self.position + 1:]
+
+    @property
+    def history(self):
+        if not self._queue:
+            raise QueueIsEmpty
+        return self._queue[:self.position]
+
+    @property
+    def length(self):
+        return len(self._queue)
+
+    def add(self, *args):
+        self._queue.extend(args)
+
     def get_next_track(self):
         if not self._queue:
             raise QueueIsEmpty
@@ -63,10 +92,15 @@ class Queue:
             return None
         return self._queue[self.position]
 
+    def empty(self):
+        self._queue.clear()
+
+
 class Player(wavelink.Player):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.queue = Queue()
+
     async def connect(self, ctx, channel=None):
         if self.is_connected:
             raise AlreadyConnectedToChannel
@@ -74,11 +108,13 @@ class Player(wavelink.Player):
             raise NoVoiceChannel
         await super().connect(channel.id)
         return channel
+
     async def teardown(self):
         try:
             await self.destroy()
         except KeyError:
             pass
+
     async def add_tracks(self, ctx, tracks):
         if not tracks:
             raise NoTracksFound
@@ -88,14 +124,11 @@ class Player(wavelink.Player):
             self.queue.add(tracks[0])
             await ctx.send(f"Added {tracks[0].title} to the queue.")
         else:
-            pass
             if (track := await self.choose_track(ctx, tracks)) is not None:
                 self.queue.add(track)
                 await ctx.send(f"Added {track.title} to the queue.")
-
-        if not self.is_playing:
-            if not self.is_playing and not self.queue.is_empty:
-                await self.start_playback()
+        if not self.is_playing and not self.queue.is_empty:
+            await self.start_playback()
 
     async def choose_track(self, ctx, tracks):
         def _check(r, u):
@@ -104,7 +137,6 @@ class Player(wavelink.Player):
                 and u == ctx.author
                 and r.message.id == msg.id
             )
-
         embed = discord.Embed(
             title="Choose a song",
             description=(
@@ -117,12 +149,11 @@ class Player(wavelink.Player):
             timestamp=dt.datetime.utcnow()
         )
         embed.set_author(name="Query Results")
-        embed.set_footer(text=f"Invoked by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
-
+        embed.set_footer(
+            text=f"Invoked by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
         msg = await ctx.send(embed=embed)
         for emoji in list(OPTIONS.keys())[:min(len(tracks), len(OPTIONS))]:
             await msg.add_reaction(emoji)
-
         try:
             reaction, _ = await self.bot.wait_for("reaction_add", timeout=60.0, check=_check)
         except asyncio.TimeoutError:
@@ -141,29 +172,36 @@ class Player(wavelink.Player):
                 await self.play(track)
         except QueueIsEmpty:
             pass
+
+
 class Music(commands.Cog, wavelink.WavelinkMixin):
     def __init__(self, bot):
         self.bot = bot
         self.wavelink = wavelink.Client(bot=bot)
         self.bot.loop.create_task(self.start_nodes())
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if not member.bot and after.channel is None:
             if not [m for m in before.channel.members if not m.bot]:
                 await self.get_player(member.guild).teardown()
+
     @wavelink.WavelinkMixin.listener()
     async def on_node_ready(self, node):
         print(f" Wavelink node `{node.identifier}` ready.")
+
     @wavelink.WavelinkMixin.listener("on_track_stuck")
     @wavelink.WavelinkMixin.listener("on_track_end")
     @wavelink.WavelinkMixin.listener("on_track_exception")
     async def on_player_stop(self, node, payload):
         await payload.player.advance()
+
     async def cog_check(self, ctx):
         if isinstance(ctx.channel, discord.DMChannel):
             await ctx.send("Music commands are not available in DMs.")
             return False
         return True
+
     async def start_nodes(self):
         await self.bot.wait_until_ready()
         nodes = {
@@ -178,38 +216,159 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
         }
         for node in nodes.values():
             await self.wavelink.initiate_node(**node)
+
     def get_player(self, obj):
         if isinstance(obj, commands.Context):
             return self.wavelink.get_player(obj.guild.id, cls=Player, context=obj)
         elif isinstance(obj, discord.Guild):
             return self.wavelink.get_player(obj.id, cls=Player)
+
     @commands.command(name="connect", aliases=["join"])
     async def connect_command(self, ctx, *, channel: t.Optional[discord.VoiceChannel]):
         player = self.get_player(ctx)
         channel = await player.connect(ctx, channel)
         await ctx.send(f"Connected to {channel.name}.")
+
     @connect_command.error
     async def connect_command_error(self, ctx, exc):
         if isinstance(exc, AlreadyConnectedToChannel):
             await ctx.send("Already connected to a voice channel.")
         elif isinstance(exc, NoVoiceChannel):
             await ctx.send("No suitable voice channel was provided.")
+
     @commands.command(name="disconnect", aliases=["leave"])
     async def disconnect_command(self, ctx):
         player = self.get_player(ctx)
         await player.teardown()
-        await ctx.send("Disconnect.")
+        await ctx.send("Disconnected from channel")
+
     @commands.command(name="play")
     async def play_command(self, ctx, *, query: t.Optional[str]):
         player = self.get_player(ctx)
         if not player.is_connected:
             await player.connect(ctx)
         if query is None:
-            pass
+            if not player.is_paused:
+                raise PlayersIsAlreadyPlaying
+
+            if player.queue.is_empty:
+                raise QueueIsEmpty
+
+            await player.set_pause(False)
+            await ctx.send("Playback resumed")
         else:
             query = query.strip("<>")
             if not re.match(URL_REGEX, query):
                 query = f"ytsearch:{query}"
             await player.add_tracks(ctx, await self.wavelink.get_tracks(query))
+
+    @play_command.error
+    async def play_command_error(self, ctx, exc):
+        if isinstance(exc, NoTracksFound):
+            await ctx.send("No tracks were found for this")
+
+    @commands.command(name="pause")
+    async def pause(self, ctx):
+        player = self.get_player(ctx)
+        if player.is_paused:
+            raise PlayerIsAlreadyPaused
+
+        await player.set_pause(True)
+        await ctx.send("Playback paused")
+
+    @pause.error
+    async def pause__error(ctx, exc):
+        if isinstance(exc, PlayerIsAlreadyPaused):
+            await ctx.send("Player has already been paused")
+
+
+    @commands.command(name="resume")
+    async def resume(self, ctx):
+        player = self.get_player(ctx)
+
+        if player.is_playing:
+            raise PlayerIsAlreadyPlaying
+
+        await player.set_pause(False)
+        await ctx.send("Playback resumed")
+
+    @resume.error
+    async def resume_command_error(self, ctx, exc):
+        if isinstance(exc, PlayerIsAlreadyPlaying):
+            await ctx.send("Player is already playing")
+
+    @commands.command(name="stop")
+    async def stop_command(self, ctx):
+        player = self.get_player(ctx)
+        player.queue.empty()
+        await player.stop()
+        await ctx.send("Player has been stopped")
+
+    @commands.command(name="queue", pass_context=True, help="Show the queue the playlist")
+    async def queue_command(self, ctx, show: t.Optional[int] = 10):
+        player = self.get_player(ctx)
+        if player.queue.is_empty:
+            raise QueueIsEmpty
+        embed = discord.Embed(
+            title="Queue",
+            description=f"Showing next {show} tracks",
+            colour=ctx.author.colour,
+            timestamp=dt.datetime.utcnow()
+        )
+        embed.set_author(name="Query Results")
+        embed.set_footer(
+            text=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
+        embed.add_field(name="Currently Playing: ",
+                         value=player.queue.current_track.title, inline=False)
+        if upcoming := player.queue.upcoming:
+            embed.add_field(
+                name="Next up",
+                value="\n".join(t.title for t in upcoming[:show]),
+                inline=False
+            )
+        msg = await ctx.send(embed=embed)
+
+    @queue_command.error
+    async def queue_command_error(self, ctx, exc):
+        if isinstance(exc, QueueIsEmpty):
+            await ctx.send("The queue is currently empty.")
+
+
+
+    @commands.command(name="next", aliases=['skip'])
+    async def next(self, ctx):
+        player = self.get_player(ctx)
+
+        if not player.queue.upcoming:
+            raise NoMoreTracks
+
+        await player.stop()
+        await ctx.send("The next track is playing now")
+
+    @next.error
+    async def next_error(self, ctx, exc):
+        if isinstance(exc, NoMoreTracks):
+            await ctx.send("There are no more tracks in the queue")
+        if isinstance(exc, QueueIsEmpty):
+            await ctx.send("The queue is empty")
+
+    @commands.command(name="previous", aliases=['back'])
+    async def back(self, ctx):
+        player = self.get_player(ctx)
+
+        if not player.queue.history:
+            raise NoPreviousTracks
+
+        player.queue_position -= 2
+        await player.stop()
+        await ctx.send("Playing the previous song in the queue")
+
+    @back.error
+    async def next_error(self, ctx, exc):
+        if isinstance(ctx, NoMoreTracks):
+            await ctx.send("There are no previous tracks in the queue")
+        if isinstance(ctx, QueueIsEmpty):
+            await ctx.send("The queue is empty")
+
 def setup(bot):
     bot.add_cog(Music(bot))
