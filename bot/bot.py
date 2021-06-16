e0265efdc4d7d32bd89b7f5141c6fd4bca84ddb3
Tharun Viswanathan <tharun.tvishy@gmail.com>
2021-06-16 19:20:51 +0530

    Adding prefix back again

--- bot/bot.py
+++ bot/bot.py
@@ -57,7 +57,7 @@ class CloverBot(commands.Bot):
         print("Bot ready.")
 
     async def prefix(self, bot, msg):
-        return commands.when_mentioned_or("~")(bot, msg)
+        return commands.when_mentioned_or("+")(bot, msg)
 
     async def process_commands(self, msg):
         ctx = await self.get_context(msg, cls=commands.Context)
